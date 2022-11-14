67bd747c68c9711ce4b57c5dd17dea0ada985d96
yangyangjin12 <2830587056@qq.com>
2022-11-14 23:40:43 +0800

    updata

--- app/src/main/java/com/example/application2/MainActivity.java
+++ app/src/main/java/com/example/application2/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import cn.smssdk.EventHandler;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
